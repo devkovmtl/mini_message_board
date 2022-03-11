d99ebda110e68cc98f6429328326b7288ab47e59
Mikael <dev.kov.mtl@gmail.com>
2022-03-11 11:29:49 -0500

    installed nodemon and update index route and index.ejs to show message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  { text: 'Hi there!', user: 'Amando', added: new Date() },
+  { text: 'Hello World!', user: 'Charles', added: new Date() },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini MessageBoard', messages });
 });
 
 module.exports = router;
